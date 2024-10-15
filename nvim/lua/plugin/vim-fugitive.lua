12ab2287e6227a51b355fa0c9f121cd8972ff9fc
KisenT <tkisen@outlook.jp>
2024-10-16 08:51:15 +0900

    delete all

--- nvim/lua/plugin/vim-fugitive.lua
+++
@@ -1,8 +0,0 @@
-local nmap = U.keymap.nmap
-
-nmap('<localleader><space>', ':call CustomFugitive#ToggleGStatus()<cr>')
-nmap('§<space>', ':call CustomFugitive#ToggleGStatus()<cr>')
-nmap('<localleader>v', ':Gdiffsplit!<CR>')
-nmap('<localleader>h', ':diffget //2<CR>')
-nmap('<localleader>l', ':diffget //3<CR>')
-nmap('<localleader>g', ':Gclog -10 -- %<CR>')
